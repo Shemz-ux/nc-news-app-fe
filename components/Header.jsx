c121e8df0f3ece5ace7c32f23d980e36c628e32c
Shemz-ux <shemaiahnguessan@outlook.com>
2025-03-31 16:04:25 +0100

    Completed displaying all articles on home page

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 function Header(){
     return (<header>
         <div className="head">
-            <h1>NC News</h1>
+            <Link to={'/'} style={{ textDecoration: 'none', color: 'black'}}><h1>NC News</h1></Link>
             </div>
             </header>)
 }
--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import Header from '../components/Header'
 import Articles from '../components/Articles'
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Header/>
-      <Articles/>
+      <Routes>
+        <Route path="/" element={<Articles/>} />
+      </Routes>
     </>
   )
 }
