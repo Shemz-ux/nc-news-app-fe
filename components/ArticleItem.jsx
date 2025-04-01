3d581c1f4f95184167c0113ea645efc8953dc01d
Shemz-ux <shemaiahnguessan@outlook.com>
2025-04-01 12:30:31 +0100

    Completed getting associated comment for article

--- components/ArticleItem.jsx
+++ components/ArticleItem.jsx
@@ -2,13 +2,12 @@ import { useParams } from "react-router-dom"
 import { fetchArticleByID } from "../src/api";
 import useApiRequests from "../hooks/apiRequests";
 import formatDate  from "../hooks/formatDate";
+import CommentSection from "./CommentSection";
 
 function ArticleItem(){
     const {article_id} = useParams()
 
     const {data: article, loading, error } = useApiRequests(fetchArticleByID, article_id)
-
-    console.log(article)
 
     if (loading) return <p>Loading article...</p>
 
@@ -22,14 +21,17 @@ function ArticleItem(){
     return (
         <div className="read_article">
             <h2>{title}</h2>
-            <img src={article_img_url} alt={title} srcset="" />
+            <img src={article_img_url} alt={title} />
             <div className="inline">
                 <p className="author">Written by {author}</p>
                 <p className="date">{formatDate(created_at)}</p>
             </div>
             <p>{body}</p>
             <div className="like_comment"></div>
-            <div className="comments"></div>
+            <div className="comment_section">
+                <h3>Comments</h3>
+                <CommentSection article_id={article_id}/>
+            </div>
         </div>
     )
 }
