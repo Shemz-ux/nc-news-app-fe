c72cfb126513a9b8abf7a93e433fef267cd50c2b
Shemz-ux <shemaiahnguessan@outlook.com>
2025-04-03 14:46:16 +0100

    Complete delete comment ticket alongside fixes to my vote functionality

--- components/Vote.jsx
+++ components/Vote.jsx
@@ -1,19 +1,22 @@
 import { VoteContext } from "../context/VoteContext";
 import { updateArticleVotes } from "../src/api";
-import { useState, useContext } from "react";
+import { useState } from "react";
 
 function Vote({article_id, vote}){
     const [currentVotes, setCurrentVotes] = useState(0)
     const [error, setError] = useState(null)
+    const [hasVoted, setHasVoted] = useState(false)
 
 
-     const handleVote = (voted) => {
+     const handleVote = (voted) => { 
         updateArticleVotes(article_id, voted).catch(()=>{
             setCurrentVotes((current_votes)=>{
                 return current_votes - voted
             })
             setError("Your vote was not successful. Please try again!")
         })
+
+        setHasVoted(voted)
         setError(null)
         setCurrentVotes((current_votes) => current_votes + voted);
       };
@@ -21,8 +24,8 @@ function Vote({article_id, vote}){
     return (
         <>
             <p>Votes: {currentVotes + vote }</p>
-                <button onClick={() => handleVote(1)}>👍</button>
-                <button onClick={() => handleVote(-1)}>👎</button>
+                <button onClick={() => handleVote(1)} disabled={hasVoted === 1}>👍</button>
+                <button onClick={() => handleVote(-1)} disabled={hasVoted === -1}>👎</button>
             <p>{error}</p>
         </>
     )
