1f21ee30018d90b5d9a506550a145bed925a67d3
Shemz-ux <shemaiahnguessan@outlook.com>
2025-04-29 17:34:49 +0100

    Updated delete comment ui

--- components/CommentSection.jsx
+++ components/CommentSection.jsx
@@ -1,33 +1,28 @@
 import useApiRequests from "../hooks/apiRequests";
-import {formatDate, dateFormatter}from "../utils/formatDate";
+import {dateFormatter}from "../utils/formatDate";
 import { deleteComment, fetchCommentsByID } from "../src/api";
 import { useState } from "react";
 import Comment from "./Comment";
+import { MdDelete } from "react-icons/md";
 
 function CommentSection({ article_id }) {
     const { data: comments, loading, error } = useApiRequests(fetchCommentsByID, article_id);
-    const [clickedCommentId, setClickedCommentId] = useState(null);
     const [deleteMsg, setDeleteMsg] = useState(null);
-    const [enteredUsername, setEnteredUsername] = useState("");
 
-    const handleClick = (comment_id) => {
-        setClickedCommentId(comment_id);
-    };
+    // useContext for this next time
+    const loggedInUser = 'grumpy19'
 
-    const handleSubmit = (event, author) => {
-        event.preventDefault();
+    const handleSubmit = (e, comment_id) => {
+        e.preventDefault();
 
-        if (enteredUsername !== author) {
-            setDeleteMsg("Incorrect username! You can only delete your own comments.");
-        }
 
-        deleteComment(clickedCommentId).catch(()=>{
+        deleteComment(comment_id).then(()=>{setTimeout(() => location.reload(), 0)})
+        .catch(()=>{
             setDeleteMsg("A comment cannot be deleted twice!")
         })
 
         setDeleteMsg(null)
         setDeleteMsg("Comment has been successfully deleted!")
-        setEnteredUsername(""); 
     };
 
     if (loading) return <p>Loading comments section...</p>;
@@ -38,44 +33,15 @@ function CommentSection({ article_id }) {
         <div className="d-flex flex-column align-items-center mt-5">
             <h4 className="flex-end">Comments</h4>
             <Comment/>
-            {comments.map(({ comment_id, author, body, created_at, votes: comment_count }) => (
+            {comments.map(({ comment_id, author, body, created_at}) => (
                 <div class="comment_card card mb-3 mt-3" style={{width: "60%"}} key={comment_id}>
                     <div class="card-body">
                         <p class="card-text"><small class="text-body-secondary">{author} {' • '} {dateFormatter(new Date(created_at))} </small></p>
                         <p class="card-text">{body}</p>
-
+                    {loggedInUser === author ? 
+                    <MdDelete size={25} className="delete-icon mt-3" style={{cursor: "pointer"}} onClick={(e) => handleSubmit(e, comment_id)}/>: null}
                     </div>
                 </div>
-                // <div className="comment_card" key={comment_id}>
-                //     <div className="inline">
-                //         <h4 className="author">{author}</h4>
-                //         <p className="date">{formatDate(created_at)}</p>
-                //     </div>
-                //     <p>{body}</p>
-                //     <p>Votes: {comment_count}</p>
-                //     <button onClick={() => handleClick(comment_id)}>Delete</button>
-
-                //     {clickedCommentId === comment_id && (
-                //         <div>
-                //             <p>Please confirm username to delete!</p>
-                //             <div className="comment">
-                //             <form onSubmit={(event) => handleSubmit(event, author)}>
-                //                 <label htmlFor="username">Username:</label>
-                //                 <input
-                //                     type="text"
-                //                     id="username"
-                //                     placeholder="Enter username..."
-                //                     value={enteredUsername}
-                //                     onChange={(event) => setEnteredUsername(event.target.value)}
-                //                     required
-                //                 />
-                //                 <button type="submit">Confirm</button>
-                //                 {deleteMsg && <p>{deleteMsg}</p>}
-                //             </form>
-                //             </div>
-                //         </div>
-                //     )}
-                // </div>
             ))}
         </div>
     );
